c4ffc9078485626cbd5190dde7230820869f390e
fr.sanfilippo <fr.sanfilippo@gmail.com>
2013-10-03 19:15:31 +0000

    almost working (more or less)

--- Analysis_programs/tools/calc_autocorr/src/main.cpp
+++ Analysis_programs/tools/calc_autocorr/src/main.cpp
@@ -1,4 +1,137 @@
-int main()
+#ifdef HAVE_CONFIG_H
+ #include "config.hpp"
+#endif
+
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <stdlib.h>
+#include <fftw3.h>
+#include <math.h>
+
+using namespace std;
+
+const int clust_size=60;
+int jackniffed_size;
+int njacks;
+int size;
+double *data;
+
+//read allocating
+void read(const char *path)
 {
+  //scan
+  vector<double> buf;
+  ifstream fin(path);
+  double t;
+  while(fin>>t) buf.push_back(t);
+  fin.close();
+  
+  //alloc
+  njacks=buf.size()/clust_size;
+  cerr<<njacks<<endl;
+  size=njacks*clust_size;
+  data=(double*)malloc(sizeof(double)*size);
+  for(int i=0;i<size;i++) data[i]=buf[i];
+}
+
+#define EXCLUDING_LOOP(i,j,ijack)					\
+  for(int imin[2]={0,(ijack+1)*clust_size},imax[2]={ijack*clust_size,size},iter=0;iter<2;iter++) \
+    for(int j=imin[iter],i=j-iter*clust_size;j<imax[iter];j++,i++)
+
+void autocorr(double *ave_corr,double *err_corr)
+{
+  fftw_complex *in=(fftw_complex*)fftw_malloc(jackniffed_size*sizeof(fftw_complex));
+  fftw_complex *out=(fftw_complex*)fftw_malloc(jackniffed_size*sizeof(fftw_complex));
+  
+  for(int i=0;i<jackniffed_size;i++) ave_corr[i]=err_corr[i]=0;
+  
+  for(int ijack=0;ijack<njacks;ijack++)
+    {
+      //compute ave
+      double ave=0;
+      EXCLUDING_LOOP(i,j,ijack) ave+=data[j];
+      ave/=jackniffed_size;
+      
+      //copy in
+      for(int i=0;i<jackniffed_size;i++)
+	{
+	  in[i][0]=data[i]-ave;
+	  in[i][1]=0;
+	}
+      cerr<<ave<<endl;
+      
+      //take fftw
+      fftw_plan pf=fftw_plan_dft_1d(jackniffed_size,in,out,FFTW_FORWARD,FFTW_ESTIMATE);
+      fftw_execute(pf);
+      fftw_destroy_plan(pf);
+  
+      //take module
+      for(int i=0;i<jackniffed_size;i++)
+	{
+	  in[i][0]=out[i][0]*out[i][0]+out[i][1]*out[i][1];
+	  in[i][1]=0;
+	}
+
+      //take fftw
+      fftw_plan pb=fftw_plan_dft_1d(jackniffed_size,in,out,FFTW_BACKWARD,FFTW_ESTIMATE);
+      fftw_execute(pb);
+      fftw_destroy_plan(pb);
+      
+      //copy back
+      for(int i=0;i<jackniffed_size;i++)
+	{
+	  double x=out[i][0]/out[0][0];
+	  ave_corr[i]+=x;
+	  err_corr[i]+=x*x;
+	}
+    }
+  
+  //convert to ave and err
+  for(int i=0;i<jackniffed_size;i++)
+    {
+      ave_corr[i]/=njacks;
+      err_corr[i]/=njacks;
+      err_corr[i]-=ave_corr[i]*ave_corr[i];
+      err_corr[i]=sqrt(err_corr[i]*(njacks-1));
+    }
+  
+  fftw_free(in);
+  fftw_free(out);
+}
+
+int main(int narg,char **arg)
+{
+  if(narg<2)
+    {
+      cerr<<"Use: "<<arg[0]<<" filein"<<endl;
+      exit(1);
+    }
+  
+  //load
+  read(arg[1]);
+  jackniffed_size=size-clust_size;
+  
+  //compute autocorr
+  double *ave_corr=(double*)malloc(sizeof(double)*jackniffed_size);
+  double *err_corr=(double*)malloc(sizeof(double)*jackniffed_size);
+  autocorr(ave_corr,err_corr);
+  
+  int i=1;
+  double tint=0;
+  while(fabs(ave_corr[i])>err_corr[i])
+    {
+      cout<<log(i)<<" "<<ave_corr[i]<<" "<<err_corr[i]<<endl;
+      i++;
+      tint+=(ave_corr[i]+ave_corr[i-1])/2;
+    }
+  tint=2*tint+1;
+  cerr<<"tint: "<<tint<<endl;
+  
+  free(data);
+  free(ave_corr);
+  free(err_corr);
+  
   return 0;
 }
