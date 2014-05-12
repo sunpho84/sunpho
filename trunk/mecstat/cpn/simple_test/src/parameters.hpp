3a0f2499348d0d3bfd0d265285d1b543193bb2d5
fr.sanfilippo@gmail.com <fr.sanfilippo@gmail.com@677105ee-5b59-0bbc-c299-88defebd0e28>
2014-05-12 21:53:04 +0000

    fixing parameters

--- trunk/mecstat/cpn/simple_test/src/parameters.hpp
+++ trunk/mecstat/cpn/simple_test/src/parameters.hpp
@@ -14,12 +14,12 @@ const int L=72;
 //const int L=20;
 const double g=1/(N*beta);
 const double th_top=2;
-const int use_topo_pot=1;
+const int use_topo_pot=2;
 
-const int nstout_lev=1;
-const double stout_rho=0.05;
+const int nstout_lev=3;
+const double stout_rho=0.07;
 
-const double chrono_topo_coeff=0.1;
-const double chrono_topo_width=0.1;
+const double chrono_topo_coeff=0.3;
+const double chrono_topo_width=0.3;
 
 #endif
