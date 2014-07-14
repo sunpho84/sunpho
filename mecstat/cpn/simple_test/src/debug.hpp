09366a29982885465f3bd9970e446db1948eb3a9
fr.sanfilippo <fr.sanfilippo@gmail.com>
2014-07-14 17:51:12 +0000

    activated external options

--- mecstat/cpn/simple_test/src/debug.hpp
+++ mecstat/cpn/simple_test/src/debug.hpp
@@ -1,6 +1,23 @@
 #ifndef _DEBUG_HPP
 #define _DEBUG_HPP
 
+#include <chrono>
+
 void internal_crash(int line,const char *file,const char *templ,...);
+
+class timing_t
+{
+  long long int tot=0;
+  std::chrono::time_point<std::chrono::high_resolution_clock> start_moment;
+public:
+  void reset(){tot=0;}
+  timing_t(){reset();}
+  void start(){start_moment=std::chrono::high_resolution_clock::now();}
+  void stop(){tot+=std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::high_resolution_clock::now()-start_moment).count();}
+  friend ostream &operator<<(ostream &of,timing_t &t);
+};
+
+inline ostream &operator<<(ostream &of,timing_t &t)
+{of<<(double)t.tot*pow(10,-9)<<" s";return of;}
 
 #endif
--- mecstat/cpn/simple_test/src/parameters.hpp
+++ mecstat/cpn/simple_test/src/parameters.hpp
@@ -10,20 +10,18 @@
 #endif
 
 //parameters
-const int nhmc_steps=18;
 EXTERN_PARAMETERS int N;
 EXTERN_PARAMETERS double beta;
 EXTERN_PARAMETERS int L;
 EXTERN_PARAMETERS double g;
-const double th_top=0;
-const int use_topo_pot=0;
-
 EXTERN_PARAMETERS int nstout_lev;
 EXTERN_PARAMETERS double stout_rho;
-
-const int chrono_topo_after=300;
-const double chrono_topo_coeff=0.2;
-const double chrono_topo_width=0.3;
-const double chrono_topo_barr=5;
+EXTERN_PARAMETERS int nhmc_steps;
+EXTERN_PARAMETERS int use_topo_pot;
+EXTERN_PARAMETERS double th_top;
+EXTERN_PARAMETERS int chrono_topo_after;//=300;
+EXTERN_PARAMETERS double chrono_topo_coeff;//=0.2;
+EXTERN_PARAMETERS double chrono_topo_width;//=0.3;
+EXTERN_PARAMETERS double chrono_topo_barr;//=5;
 
 #endif
