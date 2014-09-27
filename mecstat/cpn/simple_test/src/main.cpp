0b7794df43a3b4cd41a01230f47dbe6051fc1c03
fr.sanfilippo <fr.sanfilippo@gmail.com>
2014-09-27 10:38:24 +0000

    added append mode - to be checked

--- mecstat/cpn/simple_test/src/main.cpp
+++ mecstat/cpn/simple_test/src/main.cpp
@@ -37,12 +37,14 @@ int main()
   init(base_isweep,read_pars);
   
   //open output files
-  ofstream energy_file("energy");
-  ofstream weight_file("topo_weight");
-  ofstream topology_file("topology");
-  ofstream corr_file("corr");
-  ofstream corrd_file("corrd");
-  ofstream mag_file("mag");
+  ios::openmode mode=ios::out;
+  if(read_pars.start_cond==LOAD) mode|=ios::app;
+  ofstream energy_file("energy",mode);
+  ofstream weight_file("topo_weight",mode);
+  ofstream topology_file("topology",mode);
+  ofstream corr_file("corr",mode);
+  ofstream corrd_file("corrd",mode);
+  ofstream mag_file("mag",mode);
   energy_file.precision(PREC);
   weight_file.precision(PREC);
   topology_file.precision(PREC);
@@ -57,53 +59,56 @@ int main()
   int isweep;
   for(isweep=base_isweep+1;isweep<=base_isweep+read_pars.nsweep;isweep++)
     {
-      //compute geometrical topological charge
-      geo_topo_time.start();
-      double topo_sim=geometric_topology_simplified(zeta);
-      geo_topo_time.stop();
-
-      //compute energy
-      energy_time.start();
-      energy_file<<isweep<<" "<<energy(zeta,lambda)/V/NDIMS<<endl;
-      energy_time.stop();
-      
-      //compute topologycal charge
-      topo_time.start();
-      stout_lambda_whole_stack(lambda_stout,stout_rho,nstout_lev,lambda);
-      for(int ilev=0;ilev<=nstout_lev;ilev++)
+      if((isweep!=base_isweep+1)||(read_pars.start_cond!=LOAD))
 	{
-	  double topo_num=topology(lambda_stout[ilev]);
-	  if(use_topo_pot==2 && ilev==nstout_lev)
-	    {
-	      double w=exp(-chrono_topo_well_tempering*compute_theta_pot(+topo_num));
-	      chrono_topo_past_values.push_back(+topo_num);
-	      chrono_topo_past_weight.push_back(w);
-	      weight_file<<w<<endl;
-	      if(isweep%DRAW_EACH==0)
-		{
-		  draw_chrono_topo_potential();
-		  draw_chrono_topo_force();
-		}
-	    }
+	  //compute geometrical topological charge
+	  geo_topo_time.start();
+	  double topo_sim=geometric_topology_simplified(zeta);
+	  geo_topo_time.stop();
 	  
-	  topology_file<<isweep<<" "<<ilev<<" "<<
-	    topo_sim<<" "<<
-	    topo_num<<endl;
+	  //compute energy
+	  energy_time.start();
+	  energy_file<<isweep<<" "<<energy(zeta,lambda)/V/NDIMS<<endl;
+	  energy_time.stop();
+	  
+	  //compute topologycal charge
+	  topo_time.start();
+	  stout_lambda_whole_stack(lambda_stout,stout_rho,nstout_lev,lambda);
+	  for(int ilev=0;ilev<=nstout_lev;ilev++)
+	    {
+	      double topo_num=topology(lambda_stout[ilev]);
+	      if(use_topo_pot==2 && ilev==nstout_lev)
+		{
+		  double w=exp(-chrono_topo_well_tempering*compute_theta_pot(+topo_num));
+		  chrono_topo_past_values.push_back(+topo_num);
+		  chrono_topo_past_weight.push_back(w);
+		  weight_file<<w<<endl;
+		  if(isweep%DRAW_EACH==0)
+		    {
+		      draw_chrono_topo_potential();
+		      draw_chrono_topo_force();
+		    }
+		}
+	      
+	      topology_file<<isweep<<" "<<ilev<<" "<<
+		topo_sim<<" "<<
+		topo_num<<endl;
+	    }
+	  topo_time.stop();
+	  
+	  //compute the correlation function
+	  if(isweep%compute_corr_each==0)
+	    {
+	      corr_time.start();
+	      double mag0,mag1,corr[L],corrd[L];
+	      compute_corr(mag0,mag1,corr,corrd,zeta);
+	      for(int i=0;i<=L/2;i++) corr_file<<isweep<<" "<<i<<" "<<corr[i]<<endl;
+	      for(int i=0;i<=L/2;i++) corrd_file<<isweep<<" "<<i/sqrt(2)<<" "<<corrd[i]<<endl;
+	      mag_file<<isweep<<" "<<mag0<<" "<<mag1<<endl;
+	      corr_time.stop();
+	    }
 	}
-      topo_time.stop();
       
-      //compute the correlation function
-      if(isweep%compute_corr_each==0)
-	{
-	  corr_time.start();
-	  double mag0,mag1,corr[L],corrd[L];
-	  compute_corr(mag0,mag1,corr,corrd,zeta);
-	  for(int i=0;i<=L/2;i++) corr_file<<isweep<<" "<<i<<" "<<corr[i]<<endl;
-	  for(int i=0;i<=L/2;i++) corrd_file<<isweep<<" "<<i/sqrt(2)<<" "<<corrd[i]<<endl;
-	  mag_file<<isweep<<" "<<mag0<<" "<<mag1<<endl;
-	  corr_time.stop();
-	}
-
       //sweep
       sweep_time.start();
       if(read_pars.use_hmc) hmc_update();
