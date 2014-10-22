6268ea45dd6121605d2589743df0aa21fc070bdf
fr.sanfilippo@gmail.com <fr.sanfilippo@gmail.com@677105ee-5b59-0bbc-c299-88defebd0e28>
2014-10-22 17:54:16 +0000

    changed to naive O(1)

--- trunk/mecstat/cpn/simple_test/src/overheat.cpp
+++ trunk/mecstat/cpn/simple_test/src/overheat.cpp
@@ -22,58 +22,31 @@ void overheat_update_site(int site)
   site_staple(staple,zeta,lambda,site);
   double staple_norm=get_zeta_norm(staple);
   
-  //new version starts here
-  
-  double theta_new=get_theta(beta*N*staple_norm,N);
-  
-  //draw a random vetor until it is not too orthogonal to the staple
-  dcomplex R[N];
-  double R_norm;
-  do
+  //if the staple is 0 we can set zeta to random
+  if(staple_norm==0) set_ON_to_rnd(zeta+site*N);
+  else
     {
-      set_ON_to_rnd(R);
-      zeta_orthogonalize_with(R,staple);
-      R_norm=get_zeta_norm(R);
-      //if(R_norm<1e-3) cout<<"drawing again site"<<endl;
-    }
-  while(R_norm<1e-3);
-  double par_comp=cos(theta_new)/staple_norm;
-  double perp_comp=sin(theta_new)/R_norm;
-  for(int n=0;n<N;n++) zeta[site*N+n]=staple[n]*par_comp+R[n]*perp_comp;
-  
-  //and ends here
-  
-  /*
-  double staple_energy=get_zeta_real_scalprod(zeta+site*N,staple);
-  
-  //compute theta in the simple way
-  double ctheta_old=staple_energy/staple_norm;
-  double theta_old=acos(ctheta_old);
-  
-  //if theta is too small we switch to alternative method
-  if(fabs(theta_old)<1.e-4) 
-    {
-      dcomplex diff[N];
-      for(int n=0;n<N;n++) diff[n]=zeta[site*N+n]-staple[n]/staple_norm;
-      theta_old=asin(get_zeta_real_scalprod(diff,diff));
-    }
-  
-  //it theta is too small the algorithm is undefined
-  if(fabs(theta_old)>5.e-8)
-    {
-      //extract theta and compute its cos
+      //generate the new theta according to the microscopic distriution of prob
       double theta_new=get_theta(beta*N*staple_norm,N);
-      double ctheta_new=cos(theta_new);
       
-      //extract remaining components
-      double a=ctheta_new/staple_norm,b=ctheta_old/staple_norm,c=sin(theta_new)/sin(theta_old);
-      for(int n=0;n<N;n++) zeta[site*N+n]=a*staple[n]-(zeta[site*N+n]-b*staple[n])*c;
+      //draw a random vetor orthogonal to the staple until it's not too small
+      dcomplex R[N];
+      double R_norm;
+      do
+	{
+	  set_ON_to_rnd(R);
+	  zeta_orthogonalize_with(R,staple);
+	  R_norm=get_zeta_norm(R);
+	}
+      while(R_norm<1e-3);
       
-      //reunitarize
-      zeta_unitarize(zeta+site*N);
+      //set the components parallel and perpendicular to the staple
+      double par_comp=cos(theta_new)/staple_norm;
+      double perp_comp=sin(theta_new)/R_norm;
+      
+      //compose the new vector
+      for(int n=0;n<N;n++) zeta[site*N+n]=staple[n]*par_comp+R[n]*perp_comp;
     }
-  else cout<<"skipping site "<<site<<": "<<theta_old<<endl;
-  */
 }
 
 //update a link using overrelaxion/heatbath
@@ -83,59 +56,32 @@ void overheat_update_link(int site,int mu)
   dcomplex staple;
   link_staple(staple,zeta,site,mu);
 
-  //compute the staple norm and energy
+  //compute the staple norm
   double staple_norm=sqrt(norm(staple));
   
-  //new version starts here
-  
-  double theta_new=get_theta_1(beta*N*staple_norm);
-  
-  //draw a random vetor until it is not too orthogonal to the staple
-  dcomplex R;
-  double R_norm;
-  do
+  //if the staple is 0 we can set zeta to random
+  if(staple_norm==0) set_U1_to_rnd(lambda[site*NDIMS]);
+  else
     {
-      set_U1_to_rnd(R);
-      lambda_orthogonalize_with(R,staple);
-      R_norm=get_lambda_norm(R);
-      //if(R_norm<1e-3) cout<<"drawing again link"<<endl;
-  }
-  while(R_norm<1e-3);
-  double par_comp=cos(theta_new)/staple_norm;
-  double perp_comp=sin(theta_new)/R_norm;
-  lambda[site*NDIMS+mu]=staple*par_comp+R*perp_comp;
-  
-  //and ends here
-  
-  /*
-  double staple_energy=get_lambda_real_scalprod(lambda[site*NDIMS+mu],staple);
-  
-  //compute theta in the simple way
-  double ctheta_old=staple_energy/staple_norm;
-  double theta_old=acos(ctheta_old);
-  
-  //if theta is too small we switch to alternative method
-  if(fabs(theta_old)<1.e-4) 
-    {
-      dcomplex diff=lambda[site*NDIMS+mu]-staple/staple_norm;
-      theta_old=asin(get_lambda_real_scalprod(diff,diff));
-    }
-  
-  //it theta is too small the algorithm is undefined
-  if(fabs(theta_old)>5.e-8)
-    {
-      //extract theta and compute its cos
+      //generate the new theta according to the microscopic distriution of prob
       double theta_new=get_theta_1(beta*N*staple_norm);
-      double ctheta_new=cos(theta_new);
       
-      //extract remaining components  
-      double a=ctheta_new/staple_norm,b=ctheta_old/staple_norm,c=sin(theta_new)/sin(theta_old);
-      lambda[site*NDIMS+mu]=a*staple-(lambda[site*NDIMS+mu]-b*staple)*c;
+      //draw a vector orthogonal to the staple until it is not too small
+      dcomplex R;
+      double R_norm;
+      do
+	{
+	  set_U1_to_rnd(R);
+	  lambda_orthogonalize_with(R,staple);
+	  R_norm=get_lambda_norm(R);
+	}
+      while(R_norm<1e-3);
       
-      //reunitarize
-      lambda_unitarize(lambda[site*NDIMS+mu]);
+      //set the components parallel and perpendicular to the staple
+      double par_comp=cos(theta_new)/staple_norm;
+      double perp_comp=sin(theta_new)/R_norm;
+      lambda[site*NDIMS+mu]=staple*par_comp+R*perp_comp;
     }
-  */
 }
 
 //sweep all the lattice with overrelaxation/heatbath
